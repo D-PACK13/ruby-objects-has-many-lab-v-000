7215577ba06b4aa1d84488abc9942073e612e70c
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-30 13:57:36 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,8 +12,8 @@ class Author
     post.author = self
   end
   
-  def add_post_by_title(post)
-    post = Post.new(post)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @posts << post
     post.author = self
   end
