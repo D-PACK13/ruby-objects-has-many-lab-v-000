59eca12b17e47740c3ece283d44d4ebdcaec911f
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-30 13:39:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,21 +4,18 @@ class Artist
   
   def initialize(name)
     @name = name
-    @song_count = 0
     @songs = []
   end
   
   def add_song(song)
     @songs << song
     song.artist = self
-    @song_count += 1
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @song_count += 1
   end
   
   def song_count
