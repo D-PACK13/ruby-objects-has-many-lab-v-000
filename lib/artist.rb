153d92856cab727656534b0fc8b0f0d1b19bf0ea
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-30 13:32:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,16 +12,19 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@soung_count +=
+    @@soung_count += 1
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@soung_count += 1
   end
   
   def song_count
+    @@song_count
+  end
   
   def songs
     @songs
